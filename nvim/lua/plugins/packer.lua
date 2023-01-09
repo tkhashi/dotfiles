c56732b98fe7dc1f34848952869fc535c303acab
Kazuhiro Takahashi <pupodenews@gmail.com>
2023-01-09 23:22:49 +0900

    Enable web-devicons

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -48,7 +48,7 @@ require('packer').startup(function(use)
   use "folke/neodev.nvim"
   use {
     'nvim-lualine/lualine.nvim', -- Fancier statusline
-    --requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use {
@@ -63,8 +63,8 @@ require('packer').startup(function(use)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  -- use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
